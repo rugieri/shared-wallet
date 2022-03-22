9b8d2ff75534b8356a4ee37e977550aae588a17a
rugieri <rugieri.ronaldo@gmail.com>
2022-03-22 17:49:07 -0300

    add allowances for external roles

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -1,8 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-contract SharedWallet {
-    function withdrawMoney(address payable _to, uint256 _amount) public {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+
+contract SharedWallet is Ownable{
+  mapping(address => uint) public allowance;
+
+  function addAllowance(address _who, uint _amount) public onlyOwner {
+    allowance[_who] = _amount;
+  }
+  
+  modifier ownerOrAllowed(uint _amount) {
+    require(msg.sender == owner() || allowance[msg.sender] >= _amount, "You are not allowed! ");
+    _;
+  }
+  }
+
+  function withdrawMoney(address payable _to, uint256 _amount) public ownerOrAllowed(_amount) {
         _to.transfer(_amount);
     }
 
