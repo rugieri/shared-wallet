a5d7ce5b771eafb5bd61d97c098f63506864a764
rugieri <rugieri.ronaldo@gmail.com>
2022-03-24 09:42:37 -0300

    add events in the allowances smart contract

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -1,23 +1,36 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract Allowance is Ownable {
+    event AllowanceChanged(
+        address indexed _forWho,
+        address indexed _byWhom,
+        uint256 _oldAmount,
+        uint256 _newAmount
+    );
+    mapping(address => uint256) public allowance;
 
-contract SharedWallet is Ownable{
-  mapping(address => uint) public allowance;
+    function setAllowance(address _who, uint256 _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+        allowance[_who] = _amount;
+    }
+}
 
-  function addAllowance(address _who, uint _amount) public onlyOwner {
-    allowance[_who] = _amount;
-  }
-  
-  modifier ownerOrAllowed(uint _amount) {
-    require(msg.sender == owner() || allowance[msg.sender] >= _amount, "You are not allowed! ");
-    _;
-  }
-  }
+contract SharedWallet is Ownable, Allowance {
+    function withdrawMoney(address payable _to, uint256 _amount)
+        public
+        ownerOrAllowed(_amount)
+    {
+        require(
+            _amount <= address(this).balance,
+            "Contract doesn't own enough money"
+        );
+        if (msg.sender != owner()) {
+            reduceAllowance(msg.sender, _amount);
+        }
 
-  function withdrawMoney(address payable _to, uint256 _amount) public ownerOrAllowed(_amount) {
         _to.transfer(_amount);
     }
 
