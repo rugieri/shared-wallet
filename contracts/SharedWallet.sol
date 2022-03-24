c14b7d4956e3b62ddf8908d9b7da410bb51c75e1
rugieri <rugieri.ronaldo@gmail.com>
2022-03-24 09:48:53 -0300

    add events in the SharedWallet smart contract

--- contracts/SharedWallet.sol
+++ contracts/SharedWallet.sol
@@ -19,6 +19,9 @@ contract Allowance is Ownable {
 }
 
 contract SharedWallet is Ownable, Allowance {
+    event MoneySent(address indexed _beneficiary, uint256 _amount);
+    event MoneyReceived(address indexed _from, uint256 _amount);
+
     function withdrawMoney(address payable _to, uint256 _amount)
         public
         ownerOrAllowed(_amount)
@@ -30,9 +33,11 @@ contract SharedWallet is Ownable, Allowance {
         if (msg.sender != owner()) {
             reduceAllowance(msg.sender, _amount);
         }
-
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
-    receive() external payable {}
+    receive() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
+    }
 }
